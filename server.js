42a82f6ca6de214767545471d4aca18caa8a48b9
Chandrasekaraa Ramadoss Ganesh <94929823+RGChandrasekaraa@users.noreply.github.com>
2024-08-31 15:43:43 +1000

    allowed origins

--- server.js
+++ server.js
@@ -19,6 +19,15 @@ dotenv.config();
 // Use PORT provided in environment
 const port = process.env.PORT;
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://2day-frontend.vercel.app");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
